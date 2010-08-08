62586c22e6e83ab640afaedabcce8e65cee8ba98
Daniel Hazelbaker <cabal95@users.noreply.github.com>
2010-08-08 01:28:52 +0000

    Begin merging the Migrator package into the Packager framework.

---
+++ Packager/Installer/PackageInstaller.cs
@@ -0,0 +1,22 @@
+﻿using RefreshCache.Packager;
+using RefreshCache.Packager.Migrator;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace RefreshCache.Packager.Installer
+{
+    class PackageInstaller
+    {
+        public SqlConnection Connection { get { return _Connection; } }
+        private SqlConnection _Connection;
+
+
+        public PackageInstaller(SqlConnection connection)
+        {
+            _Connection = connection;
+        }
+    }
+}
