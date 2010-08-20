9fa87cd78857edd4f6408edba6449e13b79b214d
Daniel Hazelbaker <cabal95@users.noreply.github.com>
2010-08-20 02:45:09 +0000

    Add in support code for implementing PackageInfo into the Package class. Implement the PackageInfo class. Implement all the various dependencies classes. Updated the documentation file.

---
+++ Packager/Extensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+
+namespace RefreshCache.Packager
+{
+    /// <summary>
+    /// Extension to the XmlNode class that provides helpful methods when
+    /// dealing with nodes.
+    /// </summary>
+    public static class XmlNodeExtensions
+    {
+        /// <summary>
+        /// Create a copy of a node and rename it. Useful since the Name
+        /// property is read-only. Grumble Grumble Grumble.
+        /// </summary>
+        /// <param name="node">The node to be renamed.</param>
+        /// <param name="newName">The new name of the node.</param>
+        /// <returns>A copy of the node which has the new name.</returns>
+        public static XmlNode CopyAndRename(this XmlNode node, String newName)
+        {
+            XmlDocument doc = node.OwnerDocument;
+            XmlNode newNode = doc.CreateNode(node.NodeType, newName, null);
+
+            while (node.HasChildNodes)
+            {
+                newNode.AppendChild(node.FirstChild);
+            }
+
+            XmlAttributeCollection ac = node.Attributes;
+            while (ac.Count > 0)
+            {
+                newNode.Attributes.Append(ac[0]);
+            }
+
+            XmlNode parent = node.ParentNode;
+            if (parent != null)
+            {
+                parent.ReplaceChild(newNode, node);
+            }
+
+            return newNode;
+        }
+    }
+}
--- Packager/PackageInfo.cs
+++ Packager/PackageInfo.cs
@@ -3,70 +3,215 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace RefreshCache.Packager
 {
+    /// <summary>
+    /// Defines information about the package that can be displayed
+    /// to the user. It also defines information about what the package
+    /// needs to be installed properly.
+    /// </summary>
     public class PackageInfo
     {
-        public String Distributor { get; set; }
-        public String PackageName { get; set; }
-        public PackageVersion Version { get; set; }
-        public String Synopsis { get; set; }
-        public String Description { get; set; }
-        public PackageVersionRequirement Arena { get; set; }
+        #region Properties
 
+        /// <summary>
+        /// The name of the church (or group) distributing this package.
+        /// </summary>
+        /// <example>"Refresh Cache"</example>
+        public String Distributor { get; set; }
+
+        /// <summary>
+        /// The full name of the package, including the distributor short
+        /// name.
+        /// </summary>
+        /// <example>"RC.Package Manager"</example>
+        public String PackageName { get; set; }
+
+        /// <summary>
+        /// The version of this package.
+        /// </summary>
+        public PackageVersion Version { get; set; }
+
+        /// <summary>
+        /// A short one sentence statement of what this package is for
+        /// and what it does. This is displayed to the user before they
+        /// view the full details of the package. It should give them
+        /// a good idea of what they are getting.
+        /// </summary>
+        public String Synopsis { get; set; }
+
+        /// <summary>
+        /// A full paragraph describing the package and what it provides
+        /// to Arena. This is currently a plain text only field, though
+        /// it may be expanded later to support some simple html tags
+        /// for things like bullet points.
+        /// </summary>
+        public String Description { get; set; }
+
+        /// <summary>
+        /// The version requirements of Arena for this package to be
+        /// installed. If this property is null then the package does
+        /// not have any Arena version requirement whatsoever.
+        /// </summary>
+        /// <remarks>
+        /// Most packages should include at least a minimum version
+        /// number identifying the lowest version number the distributor
+        /// is willing to support.
+        /// </remarks>
+        public VersionRequirement Arena { get; set; }
+
+        /// <summary>
+        /// The list of package dependencies that must be installed for
+        /// this package to be installed and function properly. Each
+        /// PackageRequirement must be met for the package to be installed.
+        /// </summary>
         public List<PackageRequirement> Requires { get { return _Requires; } }
         private List<PackageRequirement> _Requires;
 
+        /// <summary>
+        /// The list of packages that are recommended to be installed along
+        /// with this package. A recommendation can be ignored, but the user
+        /// will be informed of all recommendations and be given the chance
+        /// to install each of them.
+        /// </summary>
         public List<PackageRecommendation> Recommends { get { return _Recommends; } }
         private List<PackageRecommendation> _Recommends;
 
+        /// <summary>
+        /// The change log information for this package. Each entry specifies
+        /// the changes that were made when the specified version was released.
+        /// This collection is used to show the user all changes that were made
+        /// to the package before the begin an upgrade process.
+        /// </summary>
         public List<PackageChangelog> Changelog { get { return _Changelog; } }
         private List<PackageChangelog> _Changelog;
-    }
 
-    public class PackageVersionRequirement
-    {
-        public PackageVersion MinVersion { get; set; }
-        public PackageVersion MaxVersion { get; set; }
-        public PackageVersion Version { get; set; }
+        #endregion
 
-        public void ValidateVersion(PackageVersion version, String prefix)
+
+        #region Constructors
+
+        /// <summary>
+        /// Create a new, empty, PackageInfo instance. This method is used when
+        /// a new package is created.
+        /// </summary>
+        public PackageInfo()
         {
-            if (MinVersion != null && MinVersion.CompareTo(version) >= 0)
+            _Requires = new List<PackageRequirement>();
+            _Recommends = new List<PackageRecommendation>();
+            _Changelog = new List<PackageChangelog>();
+        }
+
+
+        /// <summary>
+        /// Load a previously saved PackageInfo instance from the XML
+        /// node.
+        /// </summary>
+        /// <param name="node">The node that is the result of a previous call to Save().</param>
+        public PackageInfo(XmlNode node)
+            : this()
+        {
+            XmlNode n;
+
+
+            //
+            // Load all the required properties.
+            //
+            Distributor = node.SelectSingleNode("child::Distributor").InnerText;
+            PackageName = node.SelectSingleNode("child::PackageName").InnerText;
+            Version = new PackageVersion(node.SelectSingleNode("child::Version").InnerText);
+            Synopsis = node.SelectSingleNode("child::Synopsis").InnerText;
+            Description = node.SelectSingleNode("child::Description").InnerText;
+
+            //
+            // Load all the optional single-value properties.
+            //
+            if ((n = node.SelectSingleNode("child::ArenaVersion")) != null)
             {
-                throw new PackageDependencyException(String.Format(
-                    "{0} version {1} does not meet minimum version requirement of {2}",
-                    prefix, version, MinVersion));
+                Arena = new VersionRequirement(n);
             }
-            else if (MaxVersion != null && MaxVersion.CompareTo(version) <= 0)
+
+            //
+            // Load all the optional multi-value properties.
+            //
+            foreach (XmlNode mv in node.SelectNodes("child::Require"))
             {
-                throw new PackageDependencyException(String.Format(
-                    "{0} version {1} exceeds maximum version requirement of {2}",
-                    prefix, version, MaxVersion));
+                Requires.Add(new PackageRequirement(mv));
             }
-            else if (Version != null && Version.CompareTo(version) == 0)
+            foreach (XmlNode mv in node.SelectNodes("child::Recommend"))
             {
-                throw new PackageDependencyException(String.Format(
-                    "{0} version {1} does not meet exact version requirement of {2}",
-                    prefix, version, Version));
+                Recommends.Add(new PackageRecommendation(mv));
+            }
+            foreach (XmlNode mv in node.SelectNodes("child::Changelog"))
+            {
+                Changelog.Add(new PackageChangelog(mv));
             }
         }
-    }
 
-    public class PackageRequirement : PackageVersionRequirement
-    {
-        public String Name { get; set; }
-    }
+        #endregion
 
-    public class PackageRecommendation : PackageRequirement
-    {
-        public String Description { get; set; }
-    }
 
-    public class PackageChangelog
-    {
-        public PackageVersion Version { get; set; }
-        public String Description { get; set; }
+        /// <summary>
+        /// Save the information identified in this PackageInfo instance
+        /// into an XmlNode that can be stored on disk and reloaded later.
+        /// </summary>
+        /// <param name="doc">The document that the new node will be stored in.</param>
+        /// <param name="isExport">Specifies wether or not this save operation is a full export.</param>
+        /// <returns>A new XmlNode which contains all the supplied information about this package.</returns>
+        public XmlNode Save(XmlDocument doc, Boolean isExport)
+        {
+            XmlNode node, n;
+
+
+            node = doc.CreateElement("Info");
+
+            //
+            // Save all the required properties.
+            //
+            n = doc.CreateElement("Distributor");
+            n.Value = (Distributor != null ? Distributor : "");
+            node.AppendChild(n);
+            n = doc.CreateElement("PackageName");
+            n.Value = (PackageName != null ? PackageName : "");
+            node.AppendChild(n);
+            n = doc.CreateElement("Version");
+            n.Value = (Version != null ? Version.ToString() : "1.0.0");
+            node.AppendChild(n);
+            n = doc.CreateElement("Synopsis");
+            n.Value = (Synopsis != null ? Synopsis : "");
+            node.AppendChild(n);
+            n = doc.CreateElement("Description");
+            n.Value = (Description != null ? Description : "");
+            node.AppendChild(n);
+
+            //
+            // Save all the optional single-value properties.
+            //
+            if (Arena != null)
+            {
+                n = Arena.Save(doc, isExport);
+                node.AppendChild(n.CopyAndRename("ArenaVersion"));
+            }
+
+            //
+            // Save all the multi-value properties.
+            //
+            foreach (PackageRequirement req in Requires)
+            {
+                node.AppendChild(req.Save(doc, isExport));
+            }
+            foreach (PackageRecommendation rec in Recommends)
+            {
+                node.AppendChild(rec.Save(doc, isExport));
+            }
+            foreach (PackageChangelog chg in Changelog)
+            {
+                node.AppendChild(chg.Save(doc, isExport));
+            }
+
+            return node;
+        }
     }
 }
