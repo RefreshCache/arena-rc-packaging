86a8acdbb70648c5221581c1bd7e933fad90cb8f
Daniel Hazelbaker <cabal95@users.noreply.github.com>
2010-09-06 21:17:06 +0000

    Refactor the Pages tab code into new file mainFormPages.cs Refactor a little bit of code regarding the Modules tab. Add some comments to the mainForm class.

--- Package Builder/mainForm.cs
+++ Package Builder/mainForm.cs
@@ -17,6 +17,11 @@ namespace RefreshCache.Packager.Builder
         private String currentFileName = null;
         private Boolean selectionChanging = false;
 
+
+        /// <summary>
+        /// Initialize a new mainForm and prepare the user interface
+        /// for use.
+        /// </summary>
         public mainForm()
         {
             InitializeComponent();
@@ -31,87 +36,41 @@ namespace RefreshCache.Packager.Builder
             //
             tcMain.SelectedIndexChanged += new EventHandler(tcMain_SelectedIndexChanged);
 
+            //
+            // Initialize and prepare all tabs for use.
+            //
             InitTabs();
             UpdateTabs();
-
-            //
-            // Setup all the page tab actions and events.
-            //
-            tvPages.BeforeSelect += new TreeViewCancelEventHandler(tvPages_BeforeSelect);
-            tvPages.AfterSelect += new TreeViewEventHandler(tvPages_AfterSelect);
-            tbPageName.TextChanged += new EventHandler(tbPageName_TextChanged);
-            cbPageDisplayInNav.CheckedChanged += new EventHandler(cbPageDisplayInNav_CheckedChanged);
-            cbPageRequireSSL.CheckedChanged += new EventHandler(cbPageRequireSSL_CheckedChanged);
-            cbPageValidateRequest.CheckedChanged += new EventHandler(cbPageValidateRequest_CheckedChanged);
-            tbPageDescription.TextChanged += new EventHandler(tbPageDescription_TextChanged);
-            tbPageGuid.TextChanged += new EventHandler(tbPageGuid_TextChanged);
-            dgPageSettings.VirtualMode = true;
-            dgPageSettings.CellValueNeeded += new DataGridViewCellValueEventHandler(dgPageSettings_CellValueNeeded);
-            dgPageSettings.CellValuePushed += new DataGridViewCellValueEventHandler(dgPageSettings_CellValuePushed);
-            dgPageSettings.RowCount = 0;
-            tbModuleInstanceTitle.TextChanged += new EventHandler(tbModuleInstanceTitle_TextChanged);
-            tbModuleInstanceTemplateFrameName.TextChanged += new EventHandler(tbModuleInstanceTemplateFrameName_TextChanged);
-            tbModuleInstanceDetails.TextChanged += new EventHandler(tbModuleInstanceDetails_TextChanged);
-            cbModuleInstanceShowTitle.CheckedChanged += new EventHandler(cbModuleInstanceShowTitle_CheckedChanged);
-            cbModuleInstanceType.SelectedValueChanged += new EventHandler(cbModuleInstanceType_SelectedValueChanged);
-            DataGridViewComboBoxColumn box = (DataGridViewComboBoxColumn)dgModuleInstanceSettings.Columns["Type"];
-            box.Items.AddRange(Enum.GetNames(typeof(ModuleInstanceSettingType)));
-            box.Sorted = true;
-            dgModuleInstanceSettings.VirtualMode = true;
-            dgModuleInstanceSettings.CellValueNeeded += new DataGridViewCellValueEventHandler(dgModuleInstanceSettings_CellValueNeeded);
-            dgModuleInstanceSettings.CellValuePushed += new DataGridViewCellValueEventHandler(dgModuleInstanceSettings_CellValuePushed);
-            dgModuleInstanceSettings.UserDeletingRow += new DataGridViewRowCancelEventHandler(dgModuleInstanceSettings_UserDeletingRow);
-            dgModuleInstanceSettings.RowCount = 1;
         }
 
 
         #region Helper methods
 
+        /// <summary>
+        /// Initialize all the tabs and prepare them for use.
+        /// </summary>
         private void InitTabs()
         {
             InitInfoTab();
             InitRequirementsTab();
             InitVersionsTab();
             InitFilesTab();
+            InitModulesTab();
+            InitPagesTab();
         }
 
 
+        /// <summary>
+        /// Update all the user interface elements when a package is loaded.
+        /// </summary>
         private void UpdateTabs()
         {
             UpdateInfoTab();
             UpdateRequirementsTab();
             UpdateVersionsTab();
             UpdateFilesTab();
-        }
-
-        private PageInstance SelectedPageInstance()
-        {
-            if (tvPages.SelectedNode == null ||
-                typeof(PageInstance).IsAssignableFrom(tvPages.SelectedNode.Tag.GetType()) == false)
-            {
-                return null;
-            }
-
-            return (PageInstance)tvPages.SelectedNode.Tag;
-        }
-
-        private ModuleInstance SelectedModuleInstance()
-        {
-            if (tvPages.SelectedNode == null ||
-                typeof(ModuleInstance).IsAssignableFrom(tvPages.SelectedNode.Tag.GetType()) == false)
-            {
-                return null;
-            }
-
-            return (ModuleInstance)tvPages.SelectedNode.Tag;
-        }
-
-        private Module SelectedModule()
-        {
-            if (dgModules.SelectedCells.Count == 0)
-                return null;
-
-            return package.Modules[dgModules.SelectedCells[0].RowIndex];
+            UpdateModulesTab();
+            UpdatePagesTab();
         }
 
         //
@@ -135,323 +94,8 @@ namespace RefreshCache.Packager.Builder
             return Encoding.UTF8.GetString(memoryStream.ToArray());
         }
 
-        private TreeNode TreeNodeFromPageInstance(PageInstance parentPage)
-        {
-            TreeNode node = new TreeNode(parentPage.PageName);
-
-
-            node.Tag = parentPage;
-
-            foreach (ModuleInstance module in parentPage.Modules)
-            {
-                TreeNode moduleNode = new TreeNode(module.ModuleTitle);
-
-                moduleNode.Tag = module;
-                node.Nodes.Add(moduleNode);
-            }
-
-            foreach (PageInstance page in parentPage.Pages)
-            {
-                TreeNode pageNode = TreeNodeFromPageInstance(page);
-
-                node.Nodes.Add(pageNode);
-            }
-
-            return node;
-        }
-
         #endregion
 
-
-        #region Pages Tab User Interface
-
-        private void btnPagesAddPage_Click(object sender, EventArgs e)
-        {
-            TreeNode node = new TreeNode();
-            PageInstance page, parentPage;
-
-
-            if (tvPages.SelectedNode == null)
-            {
-                page = new PageInstance();
-                package.Pages.Add(page);
-                tvPages.Nodes.Add(node);
-            }
-            else
-            {
-                parentPage = SelectedPageInstance();
-                if (parentPage == null)
-                    return;
-
-                page = new PageInstance();
-                parentPage.Pages.Add(page);
-                tvPages.SelectedNode.Nodes.Add(node);
-            }
-
-            node.Text = page.PageName;
-            node.Tag = page;
-            tvPages.SelectedNode = node;
-        }
-
-        private void btnPagesAddModule_Click(object sender, EventArgs e)
-        {
-            TreeNode node;
-            ModuleInstance module;
-            PageInstance parentPage = SelectedPageInstance();
-
-
-            if (parentPage == null)
-                return;
-
-            node = new TreeNode();
-            module = new ModuleInstance();
-            parentPage.Modules.Add(module);
-
-            node.Text = module.ModuleTitle;
-            node.Tag = module;
-            tvPages.SelectedNode.Nodes.Insert(parentPage.Modules.Count - 1, node);
-            tvPages.SelectedNode = node;
-        }
-
-        void tbPageGuid_TextChanged(object sender, EventArgs e)
-        {
-            PageInstance page = SelectedPageInstance();
-
-
-            if (page != null && selectionChanging == false)
-                page.Guid = tbPageGuid.Text;
-        }
-
-        void tbPageDescription_TextChanged(object sender, EventArgs e)
-        {
-            PageInstance page = SelectedPageInstance();
-
-
-            if (page != null && selectionChanging == false)
-                page.PageDescription = tbPageDescription.Text;
-        }
-
-        void cbPageValidateRequest_CheckedChanged(object sender, EventArgs e)
-        {
-            PageInstance page = SelectedPageInstance();
-
-
-            if (page != null && selectionChanging == false)
-                page.ValidateRequest = cbPageValidateRequest.Checked;
-        }
-
-        void cbPageRequireSSL_CheckedChanged(object sender, EventArgs e)
-        {
-            PageInstance page = SelectedPageInstance();
-
-
-            if (page != null && selectionChanging == false)
-                page.RequireSSL = cbPageRequireSSL.Checked;
-        }
-
-        void cbPageDisplayInNav_CheckedChanged(object sender, EventArgs e)
-        {
-            PageInstance page = SelectedPageInstance();
-
-
-            if (page != null && selectionChanging == false)
-                page.DisplayInNav = cbPageDisplayInNav.Checked;
-        }
-
-        void tbPageName_TextChanged(object sender, EventArgs e)
-        {
-            PageInstance page = SelectedPageInstance();
-
-
-            if (page != null && selectionChanging == false)
-            {
-                page.PageName = tbPageName.Text;
-                tvPages.SelectedNode.Text = page.PageName;
-            }
-        }
-
-        void tvPages_BeforeSelect(object sender, TreeViewCancelEventArgs e)
-        {
-            dgPageSettings.RowCount = 0;
-            dgModuleInstanceSettings.RowCount = 1;
-        }
-
-        void tvPages_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            if (tvPages.SelectedNode == null)
-                return;
-
-            selectionChanging = true;
-
-            if (typeof(PageInstance).IsAssignableFrom(tvPages.SelectedNode.Tag.GetType()) == true)
-            {
-                PageInstance page = (PageInstance)tvPages.SelectedNode.Tag;
-
-                tbPageName.Text = page.PageName;
-                lbPageID.Text = "Page ID: " + page.PageID;
-                cbPageDisplayInNav.Checked = page.DisplayInNav;
-                cbPageRequireSSL.Checked = page.RequireSSL;
-                cbPageValidateRequest.Checked = page.ValidateRequest;
-                tbPageDescription.Text = page.PageDescription;
-                tbPageGuid.Text = page.Guid;
-                dgPageSettings.RowCount = 13;
-
-                tcPages.SelectedIndex = 0;
-            }
-            else if (typeof(ModuleInstance).IsAssignableFrom(tvPages.SelectedNode.Tag.GetType()) == true)
-            {
-                ModuleInstance module = (ModuleInstance)tvPages.SelectedNode.Tag;
-
-                tbModuleInstanceTitle.Text = module.ModuleTitle;
-                cbModuleInstanceShowTitle.Checked = module.ShowTitle;
-                tbModuleInstanceTemplateFrameName.Text = module.TemplateFrameName;
-                cbModuleInstanceType.SelectedValue = module.ModuleTypeID;
-                tbModuleInstanceDetails.Text = module.ModuleDetails;
-                dgModuleInstanceSettings.RowCount = module.Settings.Count + 1;
-
-                tcPages.SelectedIndex = 1;
-            }
-
-            selectionChanging = false;
-        }
-
-        void dgPageSettings_CellValuePushed(object sender, DataGridViewCellValueEventArgs e)
-        {
-            PageInstance page = (PageInstance)tvPages.SelectedNode.Tag;
-
-
-            page.Settings[e.RowIndex].Value = e.Value.ToString();
-        }
-
-        void dgPageSettings_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
-        {
-            if (tvPages.SelectedNode == null ||
-                typeof(PageInstance).IsAssignableFrom(tvPages.SelectedNode.Tag.GetType()) == false)
-            {
-                e.Value = "";
-            }
-            else
-            {
-                PageInstance page = (PageInstance)tvPages.SelectedNode.Tag;
-
-                if (e.ColumnIndex == 0)
-                    e.Value = page.Settings[e.RowIndex].DisplayName;
-                else
-                    e.Value = page.Settings[e.RowIndex].Value;
-            }
-        }
-
-        void dgModuleInstanceSettings_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
-        {
-            ModuleInstance module = SelectedModuleInstance();
-
-
-            if (module != null)
-            {
-                module.Settings.RemoveAt(e.Row.Index);
-            }
-        }
-
-        void dgModuleInstanceSettings_CellValuePushed(object sender, DataGridViewCellValueEventArgs e)
-        {
-            ModuleInstance module = SelectedModuleInstance();
-
-
-            if (module == null)
-                return;
-            if (e.RowIndex == module.Settings.Count)
-            {
-                module.Settings.Add(new ModuleInstanceSetting());
-                dgModuleInstanceSettings.UpdateCellValue(1, e.RowIndex);
-            }
-
-            if (e.ColumnIndex == 0)
-                module.Settings[e.RowIndex].Name = e.Value.ToString();
-            else if (e.ColumnIndex == 1)
-                module.Settings[e.RowIndex].Type = (ModuleInstanceSettingType)Enum.Parse(typeof(ModuleInstanceSettingType), e.Value.ToString());
-            else if (e.ColumnIndex == 2)
-                module.Settings[e.RowIndex].Value = e.Value.ToString();
-            else if (e.ColumnIndex == 3)
-                module.Settings[e.RowIndex].Guid = e.Value.ToString();
-        }
-
-        void dgModuleInstanceSettings_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
-        {
-            ModuleInstance module = SelectedModuleInstance();
-
-
-            if (module == null || e.RowIndex == module.Settings.Count)
-            {
-                return;
-            }
-
-            if (e.ColumnIndex == 0)
-                e.Value = module.Settings[e.RowIndex].Name;
-            else if (e.ColumnIndex == 1)
-                e.Value = module.Settings[e.RowIndex].Type.ToString();
-            else if (e.ColumnIndex == 2)
-                e.Value = module.Settings[e.RowIndex].Value;
-            else if (e.ColumnIndex == 3)
-                e.Value = module.Settings[e.RowIndex].Guid;
-        }
-
-        void cbModuleInstanceType_SelectedValueChanged(object sender, EventArgs e)
-        {
-            ModuleInstance instance = SelectedModuleInstance();
-
-
-            if (instance != null && selectionChanging == false)
-            {
-                instance.ModuleTypeID = Convert.ToInt32(cbModuleInstanceType.SelectedValue);
-            }
-        }
-
-        void cbModuleInstanceShowTitle_CheckedChanged(object sender, EventArgs e)
-        {
-            ModuleInstance instance = SelectedModuleInstance();
-
-
-            if (instance != null && selectionChanging == false)
-            {
-                instance.ShowTitle = cbModuleInstanceShowTitle.Checked;
-            }
-        }
-
-        void tbModuleInstanceDetails_TextChanged(object sender, EventArgs e)
-        {
-            ModuleInstance instance = SelectedModuleInstance();
-
-
-            if (instance != null && selectionChanging == false)
-            {
-                instance.ModuleDetails = tbModuleInstanceDetails.Text;
-            }
-        }
-
-        void tbModuleInstanceTemplateFrameName_TextChanged(object sender, EventArgs e)
-        {
-            ModuleInstance instance = SelectedModuleInstance();
-
-
-            if (instance != null && selectionChanging == false)
-            {
-                instance.TemplateFrameName = tbModuleInstanceTemplateFrameName.Text;
-            }
-        }
-
-        void tbModuleInstanceTitle_TextChanged(object sender, EventArgs e)
-        {
-            ModuleInstance instance = SelectedModuleInstance();
-
-
-            if (instance != null && selectionChanging == false)
-            {
-                instance.ModuleTitle = tbModuleInstanceTitle.Text;
-                tvPages.SelectedNode.Text = instance.ModuleTitle;
-            }
-        }
-
-        #endregion
 
         #region Top Level User Interface
 
@@ -471,8 +115,6 @@ namespace RefreshCache.Packager.Builder
         private void newMenu_Click(object sender, EventArgs e)
         {
             currentFileName = null;
-
-            tvPages.Nodes.Clear();
 
             package = new Package();
 
@@ -623,6 +265,7 @@ namespace RefreshCache.Packager.Builder
 
         #endregion
 
+
         #region Import/Export methods
 
         private void openFromFile(String filename)
@@ -634,16 +277,8 @@ namespace RefreshCache.Packager.Builder
             doc.Load(reader);
             reader.Close();
 
-            tvPages.Nodes.Clear();
-
             package = new Package(doc);
-
             UpdateTabs();
-
-            foreach (PageInstance page in package.Pages)
-            {
-                tvPages.Nodes.Add(TreeNodeFromPageInstance(page));
-            }
 
             currentFileName = filename;
         }
