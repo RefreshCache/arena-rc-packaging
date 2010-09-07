55ee9a33e0ca61d2020e0349bc8399d6beddb02b
cabal95 <cabal95@079d4551-f83d-4001-b0b2-43c6a34b4749>
2010-09-07 00:27:43 +0000

    New project for building CLI only apps.

---
+++ trunk/Package Builder/Cli.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+
+namespace RefreshCache.Packager.Builder
+{
+	class MainClass
+	{
+        [STAThread()]
+		public static void Main (string[] args)
+		{
+			if (args.Length != 3)
+			{
+                Console.WriteLine("Usage: pbcli.exe <source.xml> <BaseDirectory> <output.xml>");
+                Environment.Exit(-1);
+			}
+			else
+			{
+                BuildMessageCollection messages;
+                XmlWriterSettings settings;
+                XmlDocument source;
+                XmlWriter writer;
+                Package package;
+
+				//
+				// Load the source file.
+				//
+				source = new XmlDocument();
+				source.Load(args[0]);
+
+                //
+                // Load the package.
+                //
+                package = new Package(source);
+
+                //
+                // Save the package to XML.
+                //
+                messages = package.Build(args[1]);
+
+                //
+                // Check if there were any errors during the build.
+                //
+                if (package.XmlPackage == null)
+                {
+                    Console.WriteLine("Errors occured during build:");
+                    Console.WriteLine(messages.ToString());
+
+                    Environment.Exit(-1);
+                }
+
+                //
+                // Dump result.
+                //
+                settings = new XmlWriterSettings();
+                settings.Encoding = Encoding.UTF8;
+                settings.Indent = true;
+                writer = XmlWriter.Create(args[2], settings);
+                package.XmlPackage.WriteTo(writer);
+                writer.Close();
+			}
+		}
+	}
+}
+
