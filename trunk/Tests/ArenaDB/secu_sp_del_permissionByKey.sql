0c5f37b0dcfbbf52c47deec020a9f950ba07f1b2
cabal95 <cabal95@079d4551-f83d-4001-b0b2-43c6a34b4749>
2010-10-03 21:18:55 +0000

    PackageDatabase: GetInstalledPackage now properly will return null if the package was not found or had an error loading. PackageDatabase: Fixed PackagesRequiring to now call the correct stored procedure. PackageDatabase: When upgrading a package we now delete the old package from the cust_rc_packager_packages table. PackageInstaller: Modified use of ExecuteReader methods to make use of the using keyword to properly dispose of readers. InstallTest.cs: MD5FromInstalledPath now returns null if the file does not exist. InstallTest.cs: VerifyFileMD5 method now can properly check to see if a file doe snot exist. InstallTest.cs: Added DumpTable method to help in debugging. InstallTest.cs: Added GetRowCount method. InstallTest.cs: TestBasicInstall added MD5 checks on all installed files. InstallTest.cs: Added TestBasicUpgrade method to test upgrading a package. InstallTest.cs: Added TestBasicRemove method to test removing a package. BasicInstall-v1.xml: Added module_instance_guid to each module instance. secu_permission.sql: Added file. secu_sp_del_permissionByKey.sql: Added file. secu_template.sql: Added file. BasicInstall-v2.xml: Added file.

---
+++ trunk/Tests/ArenaDB/secu_sp_del_permissionByKey.sql
@@ -0,0 +1,12 @@
+﻿create proc [dbo].[secu_sp_del_permissionByKey]
+@ObjectType int,
+@ObjectKey int
+AS
+
+	DELETE secu_permission
+	WHERE [object_type] = @ObjectType
+	AND [object_key] = @ObjectKey
+ 
+
+GO
+
---
+++ trunk/Tests/ArenaDB/secu_template.sql
@@ -0,0 +1,39 @@
+﻿CREATE TABLE [dbo].[secu_template](
+	[template_id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](200) NOT NULL,
+	[description] [varchar](8000) NOT NULL,
+	[object_type] [int] NOT NULL,
+	[date_created] [datetime] NOT NULL,
+	[created_by] [varchar](50) NOT NULL,
+	[date_modified] [datetime] NOT NULL,
+	[modified_by] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_secu_template] PRIMARY KEY CLUSTERED 
+(
+	[template_id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[secu_template] ADD  CONSTRAINT [DF__secu_templ__name__7154AA7B]  DEFAULT ('') FOR [name]
+GO
+
+ALTER TABLE [dbo].[secu_template] ADD  CONSTRAINT [DF__secu_temp__descr__7248CEB4]  DEFAULT ('') FOR [description]
+GO
+
+ALTER TABLE [dbo].[secu_template] ADD  CONSTRAINT [DF__secu_temp__objec__733CF2ED]  DEFAULT ((-1)) FOR [object_type]
+GO
+
+ALTER TABLE [dbo].[secu_template] ADD  CONSTRAINT [DF__secu_temp__date___74311726]  DEFAULT (getdate()) FOR [date_created]
+GO
+
+ALTER TABLE [dbo].[secu_template] ADD  CONSTRAINT [DF__secu_temp__creat__75253B5F]  DEFAULT ('') FOR [created_by]
+GO
+
+ALTER TABLE [dbo].[secu_template] ADD  CONSTRAINT [DF__secu_temp__date___76195F98]  DEFAULT (getdate()) FOR [date_modified]
+GO
+
+ALTER TABLE [dbo].[secu_template] ADD  CONSTRAINT [DF__secu_temp__modif__770D83D1]  DEFAULT ('') FOR [modified_by]
+GO
+
+
