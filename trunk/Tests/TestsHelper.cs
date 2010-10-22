48fb948d9e3e21b2460c2bdf88d805786797426b
cabal95 <cabal95@079d4551-f83d-4001-b0b2-43c6a34b4749>
2010-10-22 05:14:38 +0000

    Added TestsHelper.cs: Provides some helper methods to Test classes. Added ModuleTests.cs: 50% unit test coverage on the Module class. Module.cs: Fixed _Files property not being set when creating an empty Module.

---
+++ trunk/Tests/TestsHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace RefreshCache.Packager.Tests
+{
+    class TestsHelper
+    {
+        protected void AddXmlAttribute(XmlNode node, String name, String value)
+        {
+            XmlAttribute xattr;
+
+
+            xattr = node.OwnerDocument.CreateAttribute(name);
+            xattr.Value = value;
+            node.Attributes.Append(xattr);
+        }
+    }
+}
